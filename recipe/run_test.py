b86d229c1d9eb9178d544cfe5d5b322e2e1d74ea
mcflugen <huttone@colorado.edu>
2017-01-13 10:25:26 -0700

    Add debug print statements.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,14 +4,22 @@ import os
 os.mkdir('_testing')
 os.chdir('_testing')
 
+print 'import csdms'
 import csdms
+print 'import Sedgrid'
 import csdms.Sedgrid
+print 'from csdms.Sedgrid import Sedgrid'
 from csdms.Sedgrid import Sedgrid
+print 'Sedgrid()'
+Sedgrid()
 
+print 'from pymt.components import Sedgrid as Model'
 from pymt.components import Sedgrid as Model
 
+print 'Model()'
 model = Model()
 
+print 'defaults'
 for default in model.defaults:
     print('{name}: {val} {units}'.format(
         name=default[0], val=default[1][0], units=default[1][1]))
