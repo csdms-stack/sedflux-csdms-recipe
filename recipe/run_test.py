b41d3cc9c99c6e1c0117fac1345ada5a9db8c1bd
mcflugen <huttone@colorado.edu>
2017-01-13 10:37:19 -0700

    Use print function from the future.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,25 +1,30 @@
 #! /usr/bin/env python
+from __future__ import print_function
+
 import os
 
 os.mkdir('_testing')
 os.chdir('_testing')
 
-print 'import csdms'
+print('import csdms')
 import csdms
-print 'import Sedgrid'
+
+print('import Sedgrid')
 import csdms.Sedgrid
-print 'from csdms.Sedgrid import Sedgrid'
+
+print('from csdms.Sedgrid import Sedgrid')
 from csdms.Sedgrid import Sedgrid
-print 'Sedgrid()'
+
+print('Sedgrid()')
 Sedgrid()
 
-print 'from pymt.components import Sedgrid as Model'
+print('from pymt.components import Sedgrid as Model')
 from pymt.components import Sedgrid as Model
 
-print 'Model()'
+print('Model()')
 model = Model()
 
-print 'defaults'
+print('defaults')
 for default in model.defaults:
     print('{name}: {val} {units}'.format(
         name=default[0], val=default[1][0], units=default[1][1]))
