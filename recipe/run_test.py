8ca47c4d5c66a457c65742dc686cb84911bbd4e1
mcflugen <huttone@colorado.edu>
2017-01-13 11:19:47 -0700

    Use bmi_factory to wrap Sedgrid.

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -5,6 +5,8 @@ os.mkdir('_testing')
 os.chdir('_testing')
 
 from pymt.babel import setup_babel_environ
+from pymt.framework.bmi_bridge import bmi_factory
+
 setup_babel_environ()
 for k, v in os.environ.items():
     print('{k}: {v}'.format(k=k, v=v))
@@ -21,11 +23,15 @@ from csdms.Sedgrid import Sedgrid
 print('Sedgrid()')
 Sedgrid()
 
+print('bmi_factory(Sedgrid)')
+bmi_factory(Sedgrid)
+
 print('from pymt.components import Sedgrid as Model')
 from pymt.components import Sedgrid as Model
 
 print('Model()')
 model = Model()
+
 
 print('defaults')
 for default in model.defaults:
